68ba08e41cbf92176b54371b25ec6813626a5e41
Brandon Richards <bqrichards@icloud.com>
2018-04-23 09:43:00 -0500

    Tallest Ladder
    Complete the Tallest Ladder challenge

---
+++ Division2_2017Challenge1.py
@@ -0,0 +1,17 @@
+''' Tallest Ladder '''
+import os
+from sys import exit
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Division2_2017Challenge1.txt')
+print(filename)
+
+lines = None
+try:
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+except IOError:
+    print('Cannot open file ' + filename + '! Exiting')
+    exit(1)
+ 
+ladders = [int(ladder.rstrip()) for ladder in lines[1:]]
+print(max(ladders))
